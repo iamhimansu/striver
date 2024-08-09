1bd35f3d15c044ddfe8b0b90c60b852544b4b920
Himanshu Raj Aman <himanshu.aman@samarth.ac.in>
2024-08-09 23:27:29 +0530

    update

---
+++ basic-maths/palindrome.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cin >> n;
+    int dup = n;
+
+    int rev = 0;
+    while (n != 0)
+    {
+        rev = (rev * 10) + n % 10;
+        n = n / 10;
+    }
+
+    if (rev == dup)
+    {
+        cout << "true";
+    }
+    else
+    {
+        cout << "false";
+    }
+}
---
+++ basic-maths/reverse-number.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ssize_t n;
+    cin >> n;
+
+    int rev={0};
+    while (n != 0)
+    {
+        rev = (rev * 10) + n % 10;
+        n = n / 10;
+    }
+    cout << rev;
+}
