96558bd6a2529f8f7eae5aa64d4c2c2f5abe1328
Himanshu Raj Aman <himanshu.aman@samarth.ac.in>
2024-08-09 22:52:12 +0530

    update

---
+++ basic-maths/count-of-digits.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n = {}, count = 0;
+
+    cin >> n;
+
+    if (n == 0)
+    {
+        n = 1;
+    }
+    while (n > 0)
+    {
+        n = n / 10;
+        count++;
+    }
+
+    cout << "There are total (" << count << ") digits.";
+}
